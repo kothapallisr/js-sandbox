535342786c896789b01de6068bee61d6fce7795a
Sridhar Kothapalli <sridr8@gmail.com>
2020-05-23 17:57:43 +0530

    app.js updated

---
+++ demo/app.js
@@ -0,0 +1,12 @@
+console.log("SAI SARATH");
+console.log("another line");
+
+for(let i=0; i<10; i++) {
+  console.log("increasing " + i);
+}
+
+let h1El = document.querySelector("h1");
+h1El.style.color = "blue";
+
+let content = document.createElement("p");
+content.innerHTML = "Hey this is the content inside of the p tag";
