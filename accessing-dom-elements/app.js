bb63214be62d06a26a3772a30978da1101879920
Sridhar Kothapalli <sridr8@gmail.com>
2020-05-25 10:23:29 +0530

    app.js

--- accessing-dom-elements/app.js
+++ accessing-dom-elements/app.js
@@ -1,7 +1,17 @@
 console.log(document.getElementById("logo"));
 
 let logoEl = document.getElementById("logo");
-
+//logoEl.style.color = "#333";
 console.log(logoEl);
 
 console.log(document.querySelector(".chore"));
+
+let choresEl = document.getElementsByName("chores");
+console.log(choresEl);
+
+let choresOptioanlEl = document.getElementsByTagName("li");
+console.log(choresOptioanlEl);
+
+let paragraphEl = document.querySelector(".random-paragraphs");
+console.log(paragraphEl);
+
