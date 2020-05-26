114246c124d08a57d3e9ac0ffac488e3c5b611b7
Sridhar Kothapalli <sridr8@gmail.com>
2020-05-26 10:41:49 +0530

    app.js updated

--- accessing-dom-elements/changing-css/app.js
+++ accessing-dom-elements/changing-css/app.js
@@ -1,1 +1,14 @@
-const bodyEl = document.querySelectorAll("body");
+const bodyEl = document.querySelector("body");
+const divEl = document.querySelector("div");
+
+bodyEl.style.background = "#f0f0f2";
+bodyEl.style.margin = "0";
+bodyEl.style.padding = "0";
+bodyEl.style.fontFamily = "sans-serif";
+
+divEl.style.width = "600px";
+divEl.style.margin = "5em auto";
+divEl.style.padding = "50px";
+divEl.style.backgroundColor = "#fff";
+divEl.style.borderRadius = "0.5em";
+
