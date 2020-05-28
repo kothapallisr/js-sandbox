feefb530238e1c6cee72ae2f568f980ab826cc11
Sridhar Kothapalli <sridr8@gmail.com>
2020-05-28 08:38:04 +0530

    nav functionality added

---
+++ accessing-dom-elements/off-canvas/app.js
@@ -0,0 +1,13 @@
+const navTriggerEl = document.querySelector(".hamburger");
+const navEl = doccument.querySelector(".nav");
+const contentEl = document.querySelector(".content");
+const hamburgerBarsEl = document.getElementsByTagName("span");
+
+function toggleNav() {
+  navTriggerEl.addEventListener("click", function(){
+    navEl.classList.toggle("open");
+    contentEl.classList.toggle("shift");
+  })
+}
+
+toggleNav();
