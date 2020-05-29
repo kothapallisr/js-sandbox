4dae9eb5430a33fee5e075f0b774d1ef7892679d
Sridhar Kothapalli <sridr8@gmail.com>
2020-05-29 23:14:24 +0530

    added js logic

--- dice_project/app.js
+++ dice_project/app.js
@@ -1,3 +1,15 @@
 const diceTriggerEl = document.querySelector(".dice__trigger");
 const diceImageEl = document.querySelector(".dice__image");
 
+function getRandomNumber() {
+  return 1 + Math.floor(Math.random() * 6);
+}
+
+function rollDice() {
+  diceTriggerEl.addEventListener("click", function() {
+    let diceNumber = getRandomNumber();
+    diceImageEl.src = "images/dice" + diceNumber + ".png";
+  })
+}
+
+rollDice();
