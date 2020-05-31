0eb0d0e3d3fefa92fb8ab567207df1642cf6f83c
Sridhar Kothapalli <sridr8@gmail.com>
2020-05-31 05:30:48 +0530

    js added

--- todo_list/app.js
+++ todo_list/app.js
@@ -1,3 +1,11 @@
 const todoInputEl = document.querySelector(".todo__input");
 const todoListEl = document.querySelector(".todo__list");
-const todoItemEl = document.querySelector(".todo__item");
+const todoItemEl = document.querySelectorAll(".todo__item");
+
+function addListItem() {
+  todoInputEl.addEventListener("keypress", function(event) {
+    console.log(event.keycode);
+  })
+}
+
+addListItem();
