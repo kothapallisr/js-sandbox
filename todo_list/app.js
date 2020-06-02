cec5849bc8a5e0d8265abb9256d15a59a9deffcb
Sridhar Kothapalli <sridr8@gmail.com>
2020-06-02 12:23:48 +0530

    add list item function implemented

--- todo_list/app.js
+++ todo_list/app.js
@@ -5,9 +5,17 @@ const todoItemEl = document.querySelectorAll(".todo__item");
 function addListItem() {
   todoInputEl.addEventListener("keypress", function(event) {
     if(event.keyCode === 13) {
-      console.log(todoInputEl.value);
+      let newListItem = createListItem(todoInputEl.value);
+      todoListEl.appendChild(newListItem);
+      todoInputEl.value = "";
     }
   })
 }
 
+function createListItem(text) {
+  const newListElement = document.createElement("li");
+  newListElement.textContent = text;
+  newListElement.setAttribute("class", "todo__item");
+  return newListElement;
+}
 addListItem();
