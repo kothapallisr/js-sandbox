e69fd7d248b12da06ae18451d6aac37a31f14921
Sridhar Kothapalli <sridr8@gmail.com>
2020-06-11 12:18:05 +0530

    prototype

--- oop_in_javascript/app.js
+++ oop_in_javascript/app.js
@@ -14,3 +14,21 @@ let car2 = new Car("Honda", "Civic");
 console.log("Constructor Function");
 console.log(car1.stats());
 console.log(car2.stats());
+
+// prototype
+
+function Bike(brand, model) {
+  this.brand = brand;
+  this.model = model;
+}
+
+Bike.prototype.stats = function() {
+  return `${this.brand} ${this.model}`;
+}
+
+let bike1 = new Bike("Honda", "CBR");
+let bike2 = new Bike("Yamaha", "R1");
+
+console.log("Prototype");
+console.log(bike1.stats());
+console.log(bike2.stats());
