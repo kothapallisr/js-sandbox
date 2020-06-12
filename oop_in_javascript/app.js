8755ddd754b96ef90f4f3dee9acc0bfd300e29ee
Sridhar Kothapalli <sridr8@gmail.com>
2020-06-12 15:16:59 +0530

    constructor

--- oop_in_javascript/app.js
+++ oop_in_javascript/app.js
@@ -39,12 +39,20 @@ function Person(name, age) {
   this.name = name;
   this.age = age;
 }
-
-
 Person.prototype.greet = function() {
   return `I'm ${this.name} and I'm ${this.age} years old!`;
 }
+function Programmer(name, age, language) {
+  Person.call(this, name, age);
+  this.language = language;
+}
+Programmer.prototype = Object.create(Person.prototype);
+Programmer.prototype.Constructor = Programmer;
 
-let person1 = new Person("John", 23);
+let coder = new Programmer("Gates", "30", "C++");
+console.log(coder.greet());
+
+const person1 = new Person("John", 23);
 
 console.log(person1.greet());
+console.log(coder.Constructor);
