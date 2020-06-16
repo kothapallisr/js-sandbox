2e85ae77e74940ac6e04986391c297c13775b997
Sridhar Kothapalli <sridr8@gmail.com>
2020-06-16 20:37:52 +0530

    random value

--- oop-problem-solving/app.js
+++ oop-problem-solving/app.js
@@ -5,3 +5,14 @@ let Ball = function(ballType){
 let b = new Ball();
 console.log(b.ballType);
 
+// color Ghost -> random color
+
+var Ghost = function() {
+  let colors = ["yellow", "green", "blue", "white", "red"];
+  let random = Math.round(Math.random() * 4);
+  this.color = colors[random];
+}
+
+let a = new Ghost();
+console.log(a.color);
+
