3276c6edfb4be376f698de65b8119e972651cb32
Sridhar Kothapalli <sridr8@gmail.com>
2020-06-20 11:45:09 +0530

    pass by value and pass by reference

--- advanced-javascript/app.js
+++ advanced-javascript/app.js
@@ -1,3 +1,19 @@
 console.log("hello".toUpperCase());
 console.log(typeof new String("hello"));
 console.log(typeof "hello");
+
+// Pass by value and Pass by reference
+
+let name = "Tom";
+let nameCopy = name;
+name += " Hardy";
+
+console.log(name);
+console.log(nameCopy); // pass by value
+
+let fruits = ["cherry", "mango"];
+let fruitscopy = fruits;  // pass by reference
+fruits.push("apple", "banana");
+
+console.log(fruits);
+console.log(fruitscopy);
