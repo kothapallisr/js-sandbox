8bb3916007fe2bc476d74b4541fcfa52bedd585c
Sridhar Kothapalli <sridr8@gmail.com>
2020-06-21 18:28:33 +0530

    callback more examples

--- advanced-javascript/callback-function/app.js
+++ advanced-javascript/callback-function/app.js
@@ -26,3 +26,19 @@ let french = (name) => {
 
 greeter("Mitchell", english);
 greeter("Didier", french);
+
+const masterZap = (arr, callback) => {
+  let result = [];
+  for(let item of arr) {
+    result.push(callback(item));
+  }
+  return result;
+}
+
+let square = (item) => Math.pow(item, 2);
+let double = (item) => item * 2;
+let uppercase = (item) => item.toUpperCase();
+
+console.log(masterZap([2,3,4], square));
+console.log(masterZap([2,3,4], double));
+console.log(masterZap(["apple", "mango", "orange"], uppercase));
