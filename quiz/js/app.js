c301315ef5946a204cef13e4a0bafdaf578e39a1
Sridhar Kothapalli <sridr8@gmail.com>
2020-07-04 20:17:45 +0530

    ES6 modules

--- quiz/js/app.js
+++ quiz/js/app.js
@@ -1,1 +1,5 @@
-// Question Function Constructor
+import greet from "./greeting.js";
+import add from "./calculate.js";
+
+greet();
+console.log(add(2, 3));
---
+++ quiz/js/greeting.js
@@ -0,0 +1,3 @@
+export default function greeting() {
+  console.log("Helloooo!");
+}
