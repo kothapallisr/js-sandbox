9e09624f40dfcad398227cfb09a004697ffddad5
Sridhar Kothapalli <sridr8@gmail.com>
2020-07-25 11:57:04 +0530

    classes, inheritance, polymorphism

---
+++ js-notes/classes.js
@@ -0,0 +1,8 @@
+/**
+ * The fact that both the inherited and overridden methods can have the 
+ * same name and co-exist is called polymorphism.
+
+   Inheritance is a powerful tool for organizing data/behavior in 
+   separate logical units (classes), but allowing the child class 
+   to cooperate with the parent by accessing/using its behavior and data.
+ */
