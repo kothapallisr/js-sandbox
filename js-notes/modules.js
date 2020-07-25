2df8135e503481f5f6d083537fe44ac49db06740
Sridhar Kothapalli <sridr8@gmail.com>
2020-07-25 12:39:16 +0530

    ES Modules

--- js-notes/modules.js
+++ js-notes/modules.js
@@ -21,4 +21,27 @@
  * We call the module factories as normal functions without using the "new" keyword.
  */
 
- 
+ /**
+  * ES MODULES
+  ES modules (ESM), introduced to the JS language in ES6, are meant to serve much the 
+  same spirit and purpose as the existing classic modules just described, 
+  especially taking into account important variations and use cases from 
+  AMD(Asynchronous Module Definition), UMD(Universal Module Definition), and CommonJS.
+
+  The implementation approach does, however, differ significantly.
+  First, there's no wrapping function to define a module. The wrapping context is a file. 
+  ESMs are always file-based; one file, one module.
+
+  Second, you don't interact with a module's "API" explicitly, but rather use the export keyword 
+  to add a variable or method to its public API definition. If something is defined in a module 
+  but not exported, then it stays hidden (just as with classic modules).
+
+  Third, and maybe most noticeably different from previously discussed patterns, 
+  you don't "instantiate" an ES module, you just import it to use its single instance. 
+  ESMs are, in effect, "singletons," in that there's only one instance ever created, 
+  at first import in your program, and all other imports just receive a reference to that
+  same single instance. If your module needs to support multiple instantiations, 
+  you have to provide a classic module-style factory function on your ESM definition 
+  for that purpose.
+  */
+
