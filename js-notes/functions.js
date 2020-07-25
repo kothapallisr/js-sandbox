d04121766087d7e163436e0633dfbc826e360c21
Sridhar Kothapalli <sridr8@gmail.com>
2020-07-25 10:08:59 +0530

    function declaration statement vs expression

---
+++ js-notes/functions.js
@@ -0,0 +1,13 @@
+/* function declaration statement */
+function awesomeFunction(coolThings) {
+  // ..
+  return amazingStuff;
+}
+
+/* function expression */
+// let awesomeFunction = ..
+// const awesomeFunction = ..
+var awesomeFunction = function(coolThings) {
+  // ..
+  return amazingStuff;
+};
