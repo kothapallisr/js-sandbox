af09e40c41477d069e821541830984a3dd37d8e3
Sridhar Kothapalli <sridr8@gmail.com>
2020-07-25 11:18:03 +0530

    coercion

---
+++ js-notes/coercion.js
@@ -0,0 +1,8 @@
+/***
+ * Coercion: Coercion means a value of one type being converted 
+ * to its respective representation in another type (to whatever extent possible).
+ * */ 
+
+ // coercive equality
+42 == "42";             // true
+1 == true;              // true
