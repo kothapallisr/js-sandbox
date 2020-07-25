50a5ae0562c66dc2efd651b35d33d757438dddd6
Sridhar Kothapalli <sridr8@gmail.com>
2020-07-25 10:37:28 +0530

    comparisons

---
+++ js-notes/comparisons.js
@@ -0,0 +1,18 @@
+console.log(3 === 3.0);              // true
+console.log("yes" === "yes");        // true
+console.log(null === null);          // true
+console.log(false === false);        // true
+console.log(42 === "42");            // false
+console.log("hello" === "Hello");    // false
+console.log(true === 1);             // false
+console.log(0 === null);             // false
+console.log("" === null);            // false
+console.log(null === undefined);     // false
+
+/* But the === operator does have some nuance to it, 
+   The === operator is designed to lie in two cases of 
+   special values: NaN and -0. Consider: */
+
+console.log(NaN === NaN); // false
+console.log(0 === -0);    // true
+                 
