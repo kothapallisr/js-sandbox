45bcfd0ebc2091dd6df30b422222e5361f761ae9
Sridhar Kothapalli <sridr8@gmail.com>
2020-07-25 10:59:58 +0530

    assignment id by reference-copy

--- js-notes/comparisons.js
+++ js-notes/comparisons.js
@@ -27,4 +27,13 @@ console.log([ 1, 2, 3 ] === [ 1, 2, 3 ]);    // false
 console.log({ a: 42 } === { a: 42 } );        // false
 console.log((x => x * 2) === (x => x * 2));   // false)
 
-                 
+var x = [ 1, 2, 3 ];
+
+// assignment is by reference-copy, so
+// y references the *same* array as x,
+// not another copy of it.
+var y = x;
+
+y === x;              // true
+y === [ 1, 2, 3 ];    // false
+x === [ 1, 2, 3 ];    // false                 
