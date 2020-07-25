4a06349aed64a685261c6cbae598c07f3ab0013e
Sridhar Kothapalli <sridr8@gmail.com>
2020-07-25 10:21:11 +0530

    functions are values

--- js-notes/functions.js
+++ js-notes/functions.js
@@ -17,3 +17,21 @@ var awesomeFunction = function(coolThings) {
   // ..
   return amazingStuff;
 };
+
+
+// Since functions are values, they can be assigned as properties on objects:
+
+var whatToSay = {
+    greeting() {
+        console.log("Hello!");
+    },
+    question() {
+        console.log("What's your name?");
+    },
+    answer() {
+        console.log("My name is Kyle.");
+    }
+};
+
+whatToSay.greeting();
+// Hello!
