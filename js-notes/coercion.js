94534a4ab0101af5fe68922ecb96cfa0454dc203
Sridhar Kothapalli <sridr8@gmail.com>
2020-07-25 11:42:35 +0530

    string comparison

--- js-notes/coercion.js
+++ js-notes/coercion.js
@@ -1,8 +1,26 @@
 /***
  * Coercion: Coercion means a value of one type being converted 
  * to its respective representation in another type (to whatever extent possible).
+ * 
+ * coercion is a core pillar of the JS language
  * */ 
 
  // coercive equality
 42 == "42";             // true
 1 == true;              // true
+
+var arr = [ "1", "10", "100", "1000" ];
+for (let i = 0; i < arr.length && arr[i] < 500; i++) { //The arr[i] < 500 invokes coercion
+    // will run 3 times
+}
+
+/*** 
+ * These relational operators typically use numeric comparisons, 
+ * except in the case where both values being compared are already strings; 
+ * in this case, they use alphabetical (dictionary-like) comparison of the strings:
+ * */
+
+var x = "10";
+var y = "9";
+
+x < y;      // true, watch out!
