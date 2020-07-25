f4a3d959e11838385bf07ea9191de290db7bda16
Sridhar Kothapalli <sridr8@gmail.com>
2020-07-25 10:11:54 +0530

    format

--- js-notes/functions.js
+++ js-notes/functions.js
@@ -1,10 +1,16 @@
-/* function declaration statement */
+/* function declaration statement 
+   The association between the identifier awesomeFunction and the function value happens
+   during the compile phase of the code, before that code is executed.
+*/
 function awesomeFunction(coolThings) {
   // ..
   return amazingStuff;
 }
 
-/* function expression */
+/* function expression 
+   a function expression is not associated with its identifier 
+   until that statement during runtime.
+*/
 // let awesomeFunction = ..
 // const awesomeFunction = ..
 var awesomeFunction = function(coolThings) {
