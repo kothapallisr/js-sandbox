34c9f54b01a37245a47376da1aa19f28e68e077e
Sridhar Kothapalli <sridr8@gmail.com>
2020-07-25 09:18:51 +0530

    parameter of catch clause

--- demo/app.js
+++ demo/app.js
@@ -18,3 +18,12 @@ function hello(myName) { // named parameter myName is treated as var
 hello("Kyle");
 myName = "Simpson";
 console.log(myName);
+
+try {
+  someError();
+}
+catch (err) {
+  console.log(err);
+  //The err is a block-scoped variable that exists only inside the catch clause, 
+  //as if it had been declared with let.
+}
