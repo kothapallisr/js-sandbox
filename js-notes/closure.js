cb3b9ad798b090565537179eee7a38ee7be19da4
Sridhar Kothapalli <sridr8@gmail.com>
2020-07-28 14:27:29 +0530

    closure example

--- js-notes/closure.js
+++ js-notes/closure.js
@@ -1,6 +1,8 @@
 /**
  * Closure is when a function remembers and continues to access its variables 
  * from outside its scope, even when the function is executed outside its scope.
+ * 
+ * When a function is defined it is attached to its enclosing scope via closure.
  */
 
  function greeting(msg) {
