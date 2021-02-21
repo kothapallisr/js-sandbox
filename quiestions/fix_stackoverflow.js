d8f9922bc43977c7d5b2e7b8152e5e20116e0f08
Sridhar Kothapalli <sridr8@gmail.com>
2021-02-21 11:10:06 +0530

    recursive code

---
+++ quiestions/fix_stackoverflow.js
@@ -0,0 +1,11 @@
+let list = new Array(6000).join(1.1).split(',');
+console.log(list);
+function removeItemsFromList() {
+  let item = list.pop();
+
+  if(item) {
+    removeItemsFromList();
+  }
+}
+removeItemsFromList();
+console.log(list);
