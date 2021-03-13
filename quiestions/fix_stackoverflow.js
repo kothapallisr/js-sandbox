da8ca0205c67b939afaf9d18a6eb7c2c71bfe85e
Sridhar Kothapalli <sridr8@gmail.com>
2021-03-13 13:26:40 +0530

    format

--- quiestions/fix_stackoverflow.js
+++ quiestions/fix_stackoverflow.js
@@ -1,15 +1,12 @@
-let list = new Array(6000).join(1.1).split(',');
+let list = new Array(6000).join(1.1).split(",");
 console.log(list);
 function removeItemsFromList() {
   let item = list.pop();
 
-  if(item) {
+  if (item) {
     removeItemsFromList();
   }
 }
 removeItemsFromList();
-<<<<<<< HEAD
+
 console.log(list);
-=======
-console.log(list);
->>>>>>> d8f9922bc43977c7d5b2e7b8152e5e20116e0f08
